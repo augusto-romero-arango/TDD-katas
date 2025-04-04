c070157490b5b641115a84c5dc0a73bf99fcf31d
Augusto Romero Arango <augusto@sinco.com.co>
2025-04-04 09:50:46 -0500

    🔴Debe_emitir_un_recibo_cuando_adiciono_un_producto

---
+++ TddKatas.SupermarketReceipt.Tests/ReciboSpecification.cs
@@ -0,0 +1,24 @@
+﻿namespace TddKatas.SupermarketReceipt.Tests;
+
+public class ReciboSpecification
+{
+    [Fact]
+    public void Debe_emitir_un_recibo_cuando_adiciono_un_producto()
+    {
+        var recibo = new Reciboo();
+        
+        recibo.Adicionar("Cepillo de dientes");
+        
+        Assert.Equal(@"Factura
+Cepillo de dientes: $3.000
+TOTAL FACTURA: $3.000", recibo.ToString());
+    }
+}
+
+public class Reciboo
+{
+    public void Adicionar(string producto)
+    {
+        throw new NotImplementedException();
+    }
+}
