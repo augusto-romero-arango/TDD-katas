f3eac0a706fc438e27bb6bc49ca406017b3470d9
Augusto Romero Arango <augusto@sinco.com.co>
2025-04-04 14:11:48 -0500

    ✅Debe_emitir_un_recibo_cuando_adiciono_un_cepillo_y_tiene_porcentaje_de_descuento

--- TddKatas.SupermarketReceipt.Tests/Recibo.cs
+++ TddKatas.SupermarketReceipt.Tests/Recibo.cs
@@ -3,12 +3,25 @@ namespace TddKatas.SupermarketReceipt.Tests;
 public class Recibo
 {
     private readonly List<string> _productosFacturados = new();
+    private readonly Dictionary<string, decimal> _descuentosAplicados = new();
 
     private readonly Dictionary<string, int> _precios = new()
     {
         {"Cepillo de dientes", 3000},
         {"Jabón", 2000}
     };
+
+    private readonly Dictionary<String, decimal> _descuentos;
+    
+
+    public Recibo()
+    {
+        _descuentos = new Dictionary<string, decimal>();
+    }
+    public Recibo(Dictionary<string, decimal> descuentos)
+    {
+        _descuentos = descuentos;
+    }
 
     public void Adicionar(string producto)
     {
@@ -17,15 +30,22 @@ public class Recibo
 
         if (!_precios.ContainsKey(producto))
             throw new ArgumentException($"El producto {producto} no existe en el sistema.");
-
+        
         _productosFacturados.Add(producto);
-    }
 
+        if (_descuentos.TryGetValue(producto, out var descuento))
+            _descuentosAplicados.Add(producto, descuento);
+    }
+    
     public override string ToString()
     {
+        var descuentos = string.Empty;
+        if(_descuentosAplicados.Any())
+            descuentos = $"{Environment.NewLine}{_descuentosAplicados.First().Key} ({_descuentosAplicados.First().Value:P0}): {_descuentosAplicados.First().Value*-1*_precios[_descuentosAplicados.First().Key]:C0}";
+        
         return $"""
                 Factura
-                {CrearDetalleProductosDelRecibo()}
+                {CrearDetalleProductosDelRecibo()}{descuentos}
                 TOTAL A PAGAR: {CalcularTotalRecibo():C0}
                 """;
     }
@@ -41,7 +61,15 @@ public class Recibo
     private int CalcularTotalRecibo()
     {
         return _productosFacturados
-            .Select(producto => _precios[producto])
+            .Select(producto =>
+            {
+                _descuentosAplicados.TryGetValue(producto, out var descuento);
+                
+                if (descuento != 0)
+                    return (int) (_precios[producto] * (1 - descuento));
+                
+                return _precios[producto];
+            })
             .Sum();
     }
 }
