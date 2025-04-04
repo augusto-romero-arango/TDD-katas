9ad382afd777aa8c1c6a8da9b3d1fc4dac8b889c
Augusto Romero Arango <augusto@sinco.com.co>
2025-04-04 12:24:07 -0500

    🔷Separación del cálculo del total de la factura de los detalles de facturación

--- TddKatas.SupermarketReceipt.Tests/ReciboSpecification.cs
+++ TddKatas.SupermarketReceipt.Tests/ReciboSpecification.cs
@@ -6,99 +6,96 @@ public class ReciboSpecification
     public void Debe_emitir_un_recibo_cuando_adiciono_un_cepillo_totalizar_en_3000()
     {
         var recibo = new Recibo();
-        
+
         recibo.Adicionar("Cepillo de dientes");
-        
+
         Assert.Equal(@"Factura
 Cepillo de dientes: $ 3.000
 TOTAL FACTURA: $ 3.000", recibo.ToString());
     }
-    
+
     [Fact]
     public void Debe_emitir_un_recibo_cuando_adiciono_un_jabon_totalizar_en_2000()
     {
         var recibo = new Recibo();
-        
+
         recibo.Adicionar("Jabón");
-        
+
         Assert.Equal(@"Factura
 Jabón: $ 2.000
 TOTAL FACTURA: $ 2.000", recibo.ToString());
     }
-    
+
     [Fact]
     public void Debe_lanzar_excepcion_cuando_adiciono_un_producto_no_existente()
     {
         var recibo = new Recibo();
-        
+
         var ex = Assert.Throws<ArgumentException>(() => recibo.Adicionar("Cerveza"));
         Assert.Equal("El producto Cerveza no existe en el sistema.", ex.Message);
     }
-    
+
     [Fact]
     public void Debe_lanzar_excepcion_cuando_adiciono_un_producto_es_vacio()
     {
         var recibo = new Recibo();
-        
+
         var ex = Assert.Throws<ArgumentException>(() => recibo.Adicionar(""));
         Assert.Equal("Debe ingresar un producto.", ex.Message);
     }
-    
+
     [Fact]
     public void Debe_emitir_un_recibo_cuando_adiciono_un_cepillo_y_un_jabon_totalizar_en_5000()
     {
         var recibo = new Recibo();
-        
+
         recibo.Adicionar("Cepillo de dientes");
         recibo.Adicionar("Jabón");
-        
+
         Assert.Equal(@"Factura
 Cepillo de dientes: $ 3.000
 Jabón: $ 2.000
 TOTAL FACTURA: $ 5.000", recibo.ToString());
     }
-
-    
 }
 
 public class Recibo
 {
-    private string? _productoFacturado;
-    private List<string> _productosFacturados = new List<string>();
-    
+    private readonly List<string> _productosFacturados = new();
+
     private readonly Dictionary<string, int> _precios = new()
     {
-        { "Cepillo de dientes", 3000 },
-        { "Jabón", 2000 }
+        {"Cepillo de dientes", 3000},
+        {"Jabón", 2000}
     };
 
     public void Adicionar(string producto)
     {
-        if(producto == "")
+        if (producto == "")
             throw new ArgumentException("Debe ingresar un producto.");
-        
+
         if (!_precios.ContainsKey(producto))
             throw new ArgumentException($"El producto {producto} no existe en el sistema.");
-        
+
         _productosFacturados.Add(producto);
     }
 
     public override string ToString()
     {
-        int totalFactura = 0;
-        var detalles = _productosFacturados.Select(producto =>
-            {
-                totalFactura += _precios[producto];
-                return $"{producto}: {_precios[producto]:C0}";
-            }
-        ).ToArray();
-        
-        var detalle = string.Join(Environment.NewLine, detalles);
-        
+        int totalFactura = _productosFacturados
+            .Select(producto => _precios[producto])
+            .Sum();
+
+        var detallePorProducto = _productosFacturados
+            .Select(producto => $"{producto}: {_precios[producto]:C0}")
+            .ToArray();
+
+        var prodcutosDetallados = string.Join(Environment.NewLine, detallePorProducto);
+
         return $"""
-               Factura
-               {detalle}
-               TOTAL FACTURA: {totalFactura:C0}
-               """;
+                Factura
+                {prodcutosDetallados}
+                TOTAL FACTURA: {totalFactura:C0}
+                """;
     }
 }
