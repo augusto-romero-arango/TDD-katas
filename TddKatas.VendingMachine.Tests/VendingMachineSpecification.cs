1d4c786e1623ffcc0de331b74a31af46b4e25ee1
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2025-04-12 09:32:55 -0500

    🔴SeleccionarProducto_CuandoHayInventario_Y_No_Hay_Dinero_Suficiente_Retorna_PRICE

--- TddKatas.VendingMachine.Tests/VendingMachineSpecification.cs
+++ TddKatas.VendingMachine.Tests/VendingMachineSpecification.cs
@@ -11,10 +11,29 @@ public class VendingMachineSpecification
         
         Assert.Equal(respuesta, new VendingMachineRespuesta("SOLD OUT"));
     }
+    
+    [Fact]
+    public void SeleccionarProducto_CuandoHayInventario_Y_No_Hay_Dinero_Suficiente_Retorna_PRICE()
+    {
+        var inventarioInicial = new List<Producto>() {Producto.Chips};
+        var maquina = new VendingMachine(inventarioInicial);
+
+        var respuesta = maquina.SelaccionarProducto(Producto.Chips); 
+        
+        Assert.Equal(respuesta, new VendingMachineRespuesta("PRICE: $ 0.50"));
+    }
 }
 
 public class VendingMachine
 {
+    public VendingMachine()
+    {
+        
+    }
+    public VendingMachine(List<Producto> inventarioInicial)
+    {
+    }
+
     public VendingMachineRespuesta SelaccionarProducto(Producto producto)
     {
         return new VendingMachineRespuesta("SOLD OUT");
