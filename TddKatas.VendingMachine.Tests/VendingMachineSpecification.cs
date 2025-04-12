3b2216bcc6a83d6ba9c650e8b146a364f648c3b5
augusto-romero-arango <a.romeroa23@uniandes.edu.co>
2025-04-12 09:22:37 -0500

    🔴SeleccionarProducto_CuandoNoHayInventario_Retorna_SOLD_OUT

---
+++ TddKatas.VendingMachine.Tests/VendingMachineSpecification.cs
@@ -0,0 +1,29 @@
+﻿namespace TddKatas.VendingMachine.Tests;
+
+public class VendingMachineSpecification
+{
+    [Fact]
+    public void SeleccionarProducto_CuandoNoHayInventario_Retorna_SOLD_OUT()
+    {
+        var maquina = new VendingMachine();
+
+        var respuesta = maquina.SelaccionarProducto(Producto.Chips); 
+        
+        Assert.Equal(respuesta, new VendingMachineRespuesta("SOLD OUT"));
+    }
+}
+
+public class VendingMachine
+{
+    public VendingMachineRespuesta SelaccionarProducto(Producto producto)
+    {
+        throw new NotImplementedException();
+    }
+}
+
+public enum Producto
+{
+    Chips
+}
+
+public record VendingMachineRespuesta(string Display);
